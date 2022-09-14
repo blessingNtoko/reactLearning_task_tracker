67499c796cdf3ec5c7b1e9a67a4295ba40d5ec06
Gundo Ntoko <gundo@purplecosmos.co.za>
2022-09-14 11:32:44 +0200

    added AddTask component

--- src/App.js
+++ src/App.js
@@ -23,11 +23,29 @@ function App() {
       reminder: true,
     },
   ]);
-  
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "There are no tasks in me"
+      )}
     </div>
   );
 }
